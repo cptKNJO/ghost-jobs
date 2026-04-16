9ec39d2cc58ffa4caff1cbf4f017d51aa99cf80d
Kazi <kazinoshin.jo@gmail.com>
2026-04-16 16:06:23 +0600

    feat: replace timestamp with timestampz

--- packages/db/src/schema-helpers.ts
+++ packages/db/src/schema-helpers.ts
@@ -1,6 +1,14 @@
 import { timestamp } from "drizzle-orm/pg-core";
 
 export const timestamps = {
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
 };

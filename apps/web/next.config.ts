51c214e7cdd507888caebc6d9ee62a0633a20082
Kazi <kazinoshin.jo@gmail.com>
2026-04-23 13:54:39 +0600

    build: ignore build errors

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@repo/ui"],
+  typescript: {
+    // TODO: Remove this after fixing
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
